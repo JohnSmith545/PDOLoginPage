f6594fb3b696bfe7ad699471e8c2d9e6393211de
JohnSmith545 <160300710+JohnSmith545@users.noreply.github.com>
2024-10-26 16:41:43 +0800

    Add files via upload

---
+++ loginpage/core/models.php
@@ -0,0 +1,85 @@
+<?php  
+
+require_once 'dbConfig.php';
+
+function insertNewUser($pdo, $username, $password) {
+
+	$checkUserSql = "SELECT * FROM user_passwords WHERE username = ?";
+	$checkUserSqlStmt = $pdo->prepare($checkUserSql);
+	$checkUserSqlStmt->execute([$username]);
+
+	if ($checkUserSqlStmt->rowCount() == 0) {
+
+		$sql = "INSERT INTO user_passwords (username,password) VALUES(?,?)";
+		$stmt = $pdo->prepare($sql);
+		$executeQuery = $stmt->execute([$username, $password]);
+
+		if ($executeQuery) {
+			$_SESSION['message'] = "User successfully inserted";
+			return true;
+		}
+
+		else {
+			$_SESSION['message'] = "An error occured from the query";
+		}
+
+	}
+	else {
+		$_SESSION['message'] = "User already exists";
+	}
+
+	
+}
+
+
+
+function loginUser($pdo, $username, $password) {
+	$sql = "SELECT * FROM user_passwords WHERE username=?";
+	$stmt = $pdo->prepare($sql);
+	$stmt->execute([$username]); 
+
+	if ($stmt->rowCount() == 1) {
+		$userInfoRow = $stmt->fetch();
+		$usernameFromDB = $userInfoRow['username']; 
+		$passwordFromDB = $userInfoRow['password'];
+
+		if ($password == $passwordFromDB) {
+			$_SESSION['username'] = $usernameFromDB;
+			$_SESSION['message'] = "Login successful!";
+			return true;
+		}
+
+		else {
+			$_SESSION['message'] = "Password is invalid, but user exists";
+		}
+	}
+
+	
+	if ($stmt->rowCount() == 0) {
+		$_SESSION['message'] = "Username doesn't exist from the database. You may consider registration first";
+	}
+
+}
+
+function getAllUsers($pdo) {
+	$sql = "SELECT * FROM user_passwords";
+	$stmt = $pdo->prepare($sql);
+	$executeQuery = $stmt->execute();
+
+	if ($executeQuery) {
+		return $stmt->fetchAll();
+	}
+
+}
+
+function getUserByID($pdo, $user_id) {
+	$sql = "SELECT * FROM user_passwords WHERE user_id = ?";
+	$stmt = $pdo->prepare($sql);
+	$executeQuery = $stmt->execute([$user_id]);
+	if ($executeQuery) {
+		return $stmt->fetch();
+	}
+}
+
+
+?>
